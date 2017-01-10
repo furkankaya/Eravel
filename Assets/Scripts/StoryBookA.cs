1206006afff58cac692f2d4f224242112f8046d2
Furkan Kaya <furkan.kaya.web@gmail.com>
2017-01-10 11:39:12 +0300

    0.79
    - Story Book level bugfixes and new paths.

--- Assets/Scripts/StoryBookA.cs
+++ Assets/Scripts/StoryBookA.cs
@@ -13,117 +13,29 @@ public class StoryBookA : MonoBehaviour {
 	public Text buttonC;
 
 	private enum States {
-		cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, corridor_0, stairs_0, stairs_1,
-		stairs_2, courtyard, floor, corridor_1, corridor_2, corridor_3,closet_door, in_closet
+		girisA, depo, tabelaA, anakapiA, girisB, tabelaB, anakapiB, koridor 
 	};
 	private States myState;
 
 	public string levelToLoad;		// bu kısım tamamlandığında yüklenecek bölüm !!! editör üzerinden yazılmalıdır
 
 	void Start () {
-		myState = States.cell;
+		myState = States.girisA;
 	}
 		
 	void Update () {
 		print (myState);
-		if 		(myState == States.cell) 		{cell();}
-		else if (myState == States.sheets_0) 	{sheets_0();}
-		else if (myState == States.sheets_1) 	{sheets_1();}
-		else if (myState == States.lock_0) 		{lock_0();}
-		else if (myState == States.lock_1) 		{lock_1();}
-		else if (myState == States.mirror) 		{mirror();}
-		else if (myState == States.cell_mirror) {cell_mirror();}
-		else if (myState == States.corridor_0) 	{corridor_0();}
-//		else if (myState == States.stairs_0) 	{stairs_0();}
-//		else if (myState == States.stairs_1) 	{stairs_1();}
-//		else if (myState == States.stairs_2) 	{stairs_2();}
-//		else if (myState == States.courtyard) 	{courtyard();}
-//		else if (myState == States.floor) 		{floor();}
-//		else if (myState == States.corridor_1) 	{corridor_1();}
-//		else if (myState == States.corridor_2) 	{corridor_2();}
-//		else if (myState == States.corridor_3) 	{corridor_3();}
-//		else if (myState == States.closet_door) {closet_door();}
-//		else if (myState == States.in_closet) 	{in_closet();}
+		if 		(myState == States.girisA) 		{girisA();}
+		else if (myState == States.tabelaA) 	{tabelaA();}
+		else if (myState == States.tabelaB) 	{tabelaB();}
+		else if (myState == States.anakapiA) 	{anakapiA();}
+		else if (myState == States.anakapiB) 	{anakapiB();}
+		else if (myState == States.depo) 		{depo();}
+		else if (myState == States.girisB) 		{girisB();}
+		else if (myState == States.koridor) 	{koridor();}
 	}
-
-//	void in_closet() {
-//		text.text = "Inside the closet you see a cleaner's uniform that looks about your size! " +
-//			"Seems like your day is looking-up.\n\n" +
-//			"Press D to Dress up, or R to Return to the corridor";
-//		if 		(Input.GetKeyDown(KeyCode.R)) 	{myState = States.corridor_2;}
-//		else if (Input.GetKeyDown(KeyCode.D)) 	{myState = States.corridor_3;}
-//	}
-//
-//	void closet_door() {
-//		text.text = "You are looking at a closet door, unfortunately it's locked. " +
-//			"Maybe you could find something around to help enourage it open?\n\n" +
-//			"Press R to Return to the corridor";
-//		if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.corridor_0;}
-//	}
-//
-//	void corridor_3() {
-//		text.text = "You're standing back in the corridor, now convincingly dressed as a cleaner. " +
-//			"You strongly consider the run for freedom.\n\n" +
-//			"Press S to take the Stairs, or U to Undress";
-//		if 		(Input.GetKeyDown(KeyCode.S)) 	{myState = States.courtyard;}
-//		else if (Input.GetKeyDown(KeyCode.U))	{myState = States.in_closet;}
-//	}
-//
-//	void corridor_2() {
-//		text.text = "Back in the corridor, having declined to dress-up as a cleaner.\n\n" +
-//			"Press C to revisit the Closet, and S to climb the stairs";
-//		if 		(Input.GetKeyDown(KeyCode.C)) 	{myState = States.in_closet;}
-//		else if (Input.GetKeyDown(KeyCode.S)) 	{myState = States.stairs_2;}
-//	}
-//
-//	void corridor_1() {
-//		text.text = "Still in the corridor. Floor still dirty. Hairclip in hand. " +
-//			"Now what? You wonder if that lock on the closet would succumb to " +
-//			"to some lock-picking?\n\n" +
-//			"P to Pick the lock, and S to climb the stairs";
-//		if (Input.GetKeyDown(KeyCode.P)) 		{myState = States.in_closet;}
-//		else if (Input.GetKeyDown(KeyCode.S)) 	{myState = States.stairs_1;}
-//	}
-//
-//	void floor () {
-//		text.text = "Rummagaing around on the dirty floor, you find a hairclip.\n\n" +
-//			"Press R to Return to the standing, or H to take the Hairclip." ;
-//		if 		(Input.GetKeyDown(KeyCode.R)) 	{myState = States.corridor_0;}
-//		else if (Input.GetKeyDown(KeyCode.H)) 	{myState = States.corridor_1;}
-//	}	
-//
-//	void courtyard () {
-//		text.text = "You walk through the courtyard dressed as a cleaner. " +
-//			"The guard tips his hat at you as you waltz past, claiming " +
-//			"your freedom. You heart races as you walk into the sunset.\n\n" +
-//			"Press P to Play again." ;
-//		if (Input.GetKeyDown(KeyCode.P)) 		{myState = States.cell;}
-//	}	
-//
-//	void stairs_0 () {
-//		text.text = "You start walking up the stairs towards the outside light. " +
-//			"You realise it's not break time, and you'll be caught immediately. " +
-//			"You slither back down the stairs and reconsider.\n\n" +
-//			"Press R to Return to the corridor." ;
-//		if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.corridor_0;}
-//	}
-//
-//	void stairs_1 () {
-//		text.text = "Unfortunately weilding a puny hairclip hasn't given you the " +
-//			"confidence to walk out into a courtyard surrounded by armed guards!\n\n" +
-//			"Press R to Retreat down the stairs" ;
-//		if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.corridor_1;}
-//	}
-//
-//	void stairs_2() {
-//		text.text = "You feel smug for picking the closet door open, and are still armed with " +
-//			"a hairclip (now badly bent). Even these achievements together don't give " +
-//			"you the courage to climb up the staris to your death!\n\n" +
-//			"Press R to Return to the corridor";
-//		if (Input.GetKeyDown(KeyCode.R)) 		{myState = States.corridor_2;}
-//	}
-
-	void cell () {
+		
+	void girisA () {
 		text.text = 
 			"Eravel giriş kapısına yaklaşıp etrafına bakındı. " +
 			"Robotlar sırayla ilerleyip ana kapıdan içeri giriyorlardı. " +
@@ -137,14 +49,14 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = "TAHTA KAPIDAN İÇERİ GİR";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonA"))
-			myState = States.sheets_0;
+			myState = States.tabelaA;
 		else if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
-			myState = States.lock_0;
+			myState = States.anakapiA;
 		else if (CrossPlatformInputManager.GetButtonDown("ButtonC"))
-			myState = States.mirror;
+			myState = States.depo;
 	}
 
-	void mirror() {
+	void depo() {
 		text.text = 
 			"Tahta kapının ardında tozlu, ufak bir oda vardı. " +
 			"Odada pek çok metal kutu, demir parçaları ve değişik aletler bulunuyordu. " +
@@ -156,12 +68,12 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = "GERİ DÖN";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonA"))
-			myState = States.cell_mirror;
+			myState = States.girisB;
 		else if (CrossPlatformInputManager.GetButtonDown("ButtonC"))
-			myState = States.cell;
+			myState = States.girisA;
 	}
 
-	void cell_mirror() {
+	void girisB() {
 		text.text = 
 			"Eravel robot parçalarını kullanarak kılık değiştirmiş halde girişe geri döndü. " +
 			"Tabela halen duvarda asılıydı. " +
@@ -172,12 +84,12 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = "ANA KAPIYA YÖNEL";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonA"))
-			myState = States.sheets_1;
+			myState = States.tabelaB;
 		else if (CrossPlatformInputManager.GetButtonDown("ButtonC"))
-			myState = States.lock_1;
+			myState = States.anakapiB;
 	}
 
-	void sheets_0 () {
+	void tabelaA () {
 		text.text =
 			"Duvarda asılı olan şey, bir tür uyarı levhasıydı. " +
 			"Eravel üzerinde yazanları okudu: " +
@@ -191,10 +103,10 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = " ";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
-			myState = States.cell;
+			myState = States.girisA;
 	}
 
-	void sheets_1() {
+	void tabelaB() {
 		text.text =
 			"Duvarda asılı olan şey, bir tür uyarı levhasıydı. " +
 			"Eravel üzerinde yazanları okudu: " +
@@ -208,10 +120,10 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = " ";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
-			myState = States.cell_mirror;
+			myState = States.girisB;
 	}
 
-	void lock_0() {
+	void anakapiA() {
 		text.text =
 			"Eravel robotların yanından ilerleyip kapının önüne geldi. " +
 			"Şansını deneyip kapıyı iteklese de bunun işe yaramayacağını biliyordu. " +
@@ -223,10 +135,10 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = " ";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
-			myState = States.cell;
+			myState = States.girisA;
 	}
 
-	void lock_1() {
+	void anakapiB() {
 		text.text = 
 			"Eravel, robotlardan oluşan sıranın en arkasına yanaşıp beklemeye koyuldu. " +
 			"Sıranın önündeki robotlar kapıdan girdikçe, diğer robotlarla birlikte o da ilerliyordu. " +
@@ -239,11 +151,10 @@ public class StoryBookA : MonoBehaviour {
 		buttonC.text = " ";
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
-			myState = States.corridor_0;
-
+			myState = States.koridor;
 	}
 
-	void corridor_0() {
+	void koridor() {
 		text.text =
 			"Robot parçalarının altında etrafını bile zar zor görebilen Eravel, kapıya ait tarama cihazlarının hemen önündeydi. " +
 			"\"GİRİŞ ONAYLANDI!\" " +
@@ -256,6 +167,5 @@ public class StoryBookA : MonoBehaviour {
 
 		if (CrossPlatformInputManager.GetButtonDown("ButtonB"))
 			SceneManager.LoadScene (levelToLoad);
-
 	}
 }
